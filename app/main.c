38b444a73932179b477c1f180e00dd7ccc1dcb6a
Hambinn <ilhambintangn@gmail.com>
2021-10-31 23:04:41 +0700

    bikin main program tp keknya belom bagus

---
+++ app/main.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../lib/boolean.h"
+
+
+void mulai(){
+  boolean endmulai = false;
+  int command;
+  while(!endmulai){
+    printf("Permainan Dimulai!\n");
+    printf("\n");
+    printf("1. MOVE\n");
+    printf("2. PICK_UP\n");
+    printf("3. DROP_OFF\n");
+    printf("4. MAP\n");
+    printf("5. TO_DO\n");
+    printf("6. IN_PROGRESS\n");
+    printf("7. BUY\n");
+    printf("8. INVENTORY\n");
+    printf("9. HELP\n");
+    printf("ENTER COMMAND:");
+    scanf("%d",&command);
+    
+
+    while((command!=1) && (command!=2 )&& (command!=3)&& (command!=4)&& (command!=5)&& (command!=6)&& (command!=7)&& (command!=8)&& (command!=9)){
+    printf("Tolong Masukkan Pilihan yang Sesuai!\n");
+    printf("1. MOVE\n");
+    printf("2. PICK_UP\n");
+    printf("3. DROP_OFF\n");
+    printf("4. MAP\n");
+    printf("5. TO_DO\n");
+    printf("6. IN_PROGRESS\n");
+    printf("7. BUY\n");
+    printf("8. INVENTORY\n");
+    printf("9. HELP\n");
+    printf("ENTER COMMAND:");
+    scanf("%d",&command);
+    }
+
+    if(command == 1){
+      //move
+    }else if(command == 2){
+      // pick_up
+    }else if(command == 3){
+      // drop_off
+    }else if(command ==4){
+      // map
+    }else if(command == 5){
+      // to-do
+    }else if(command == 6){
+      // in_progress
+    }else if(command == 7){
+      // buy
+    }else if(command == 8){
+      // inventory
+    }else if(command == 9){
+      // help
+    }
+  }
+}
+
+int main(){
+  boolean end = false;
+  int pilihanMain;
+  char fileName[30];
+  while(!end){
+    printf("Welcome to Kurir Mobita!\n");
+    printf("\n");
+    printf("Masukkan Pilihan Anda:\n");
+    printf("1. NEW GAME\n");
+    printf("2. LOAD GAME\n");
+    printf("3. EXIT\n");
+
+    scanf("%d",&pilihanMain);
+    while((pilihanMain!=1) && (pilihanMain!=2 )&& (pilihanMain!=3)){
+      printf("Tolong Masukkan Pilihan yang Sesuai!\n");
+      printf("1. NEW GAME\n");
+      printf("2. LOAD GAME\n");
+      printf("3. EXIT\n");
+      scanf("%d",&pilihanMain);
+    }
+
+    if(pilihanMain == 1){
+      scanf("%s",fileName);
+      // gatau cara cek nama filenya bener atau ga
+    }else if(pilihanMain ==3){
+      printf("Terimakasih Sudah Bermain!");
+      return 0;
+    }else{
+      scanf("%s",fileName);
+      // gatau cara cek nama filenya bener atau ga
+    }
+ 
+  }
+
+  return 0;
+}
